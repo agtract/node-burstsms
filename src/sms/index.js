ee9b4d7cd0a69e18f4771deedfb5cb0ea161713a
James West <james@jameswest.co.nz>
2016-05-20 12:07:14 +1200

    Added all send options

--- src/sms/index.js
+++ src/sms/index.js
@@ -6,10 +6,19 @@ module.exports = function (config) {
 
   self.methods = {
     // .send()
-    send: function (message, to) {
+    send: function (message, to, from, sendAt, listId, dlrCallback, replyCallback, validity, repliesToEmail, fromShared, countrycode) {
       var params = {
         message: message,
-        to: to
+        to: to,
+        from: from,
+        "send_at": sendAt,
+        "list_id": listId,
+        "dlr_callback": dlrCallback,
+        "reply_callback": replyCallback,
+        validity: validity,
+        "replies_to_email": repliesToEmail,
+        "from_shared": fromShared,
+        countrycode: countrycode
       }
       return request('send-sms', params, config)
     },
